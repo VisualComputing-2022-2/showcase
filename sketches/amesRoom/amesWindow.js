1f8056ecba4e1fe5b6293aca6e770e1e19751dd6
judgonzalezmu <judgonzalezmu@users.noreply.github.com>
2022-10-19 14:39:28 +0000

    deploy: 3efdb3172b3b206704cba9f8af48258e21acbe6e

--- sketches/amesRoom/amesWindow.js
+++ sketches/amesRoom/amesWindow.js
@@ -11,7 +11,7 @@ function setup() {
 
 function draw() {
   background(255);  
-  rotateY(frameCount * 0.01);
+  rotateY(frameCount * 0.03);
   translate(50,0)
   rect(-300, -200, 600, 400);
   noStroke();
