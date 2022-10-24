7e75c43409bcb927cedb8db34083fe4d4f34c39a
Nicolas Sanchez Vargas <nisanchezva@unal.edu.co>
2022-10-23 20:35:10 -0500

    Ligeros cambios

--- content/sketches/amesRoom/amesRoom.js
+++ content/sketches/amesRoom/amesRoom.js
@@ -1,13 +1,72 @@
 const rotate_x_matrix = [];
-var angle=3;
+var x=0;
+var y=0;
+var z=0;
+var x2 = 0;
+var y2 = 0;
+var z2 = 0;
+
 var tallBoxes=true;
 var tallCones=false;
 var tallTorus=false;
 var compare=false;
 
+//Paredes
+var leftW=true;
+var rightW=true;
+var floorH=true;
+var ceiling=true;
+var back=true;
+
+var movement=false;
+//Botones 
+let buttonL;
+let buttonR;
+let buttonB;
+let buttonC;
+let buttonF;
+
+let buttonM;
+
 function setup() {
     createCanvas(600, 400,WEBGL);
-    createEasyCam({distance : 150, center:[0,-46,0]});
+    createEasyCam({distance : 150, center:[0,-50,0], rotation:[1,0.11, 0, 0]});
+    document.oncontextmenu = ()=>false;
+    
+  
+    //Botones
+    //Techo
+    buttonC = createButton('');
+    buttonC.position(550,5);
+    buttonC.size(10,10);
+    buttonC.mousePressed(changeCeiling);
+    //Izquierda
+    buttonL = createButton('');
+    buttonL.position(530,17);
+    buttonL.size(10,10);
+    buttonL.mousePressed(changeLeft);
+    //Derecha
+    buttonR = createButton('');
+    buttonR.position(570,17);
+    buttonR.size(10,10);
+    buttonR.mousePressed(changeRight);
+    //Fondo
+    buttonB = createButton('');
+    buttonB.position(550,17);
+    buttonB.size(10,10);
+    buttonB.mousePressed(changeBack);
+    //Piso
+    buttonF = createButton('');
+    buttonF.position(550,29);
+    buttonF.size(10,10);
+    buttonF.mousePressed(changeFloor);
+  
+  
+    //Movimiento
+    buttonF = createButton('Movimiento');
+    buttonF.position(515,55);
+    buttonF.size(80,30);
+    buttonF.mousePressed(changeMovement);
 }
 
 function rotateXaxis(angle){  
@@ -31,7 +90,9 @@ function draw() {
     translate(-50,-50,0);
 
 
+  
     //piso
+    if(floorH){
     fill(255);
     stroke(0);
     beginShape(); 
@@ -40,8 +101,30 @@ function draw() {
     vertex(100,100,0);
     vertex(0,100,0);
     endShape();
+      
+    //lineas del piso
+    line(0, 0, 0, 0, 100, 0)
+    line(20,17, 2.5, 25, 100, 0)
+    line(40,30, 5, 50, 100, 0)
+    line(60, 45, 7.5, 75, 100, 0)
+      
+    }
     
+    //techo
+    if(ceiling){
+    fill(100,211,230);
+    stroke(0);
+    beginShape(); 
+    vertex(0,0,100);
+    vertex(80,60,80);
+    vertex(100,100,100);
+    vertex(0,100,100);
+    endShape();
+    }
+  
+  
     //izq
+    if(leftW){
     fill(168,218,220);
     stroke(0);
     beginShape(); 
@@ -50,34 +133,36 @@ function draw() {
     vertex(0,100,100);
     vertex(0,0,100); 
     endShape();
+    }
 
     //derecha
+    if(rightW){
     fill(168,218,220);
     stroke(0);
     beginShape(); 
     vertex(100,100,0);
     vertex(80,60,10);
-    vertex(80,60,90);
+    vertex(80,60,80);
     vertex(100,100,100);
     endShape();
+    }
 
     //fondo
+    if(back){
     fill(168,218,220);
     stroke(0);
     beginShape(); 
     vertex(0,0,0);
     vertex(80,60,10);
-    vertex(80,60,90);
+    vertex(80,60,80);
     vertex(0,0,100);
     endShape();
-
     line(0, 0, 0, 0, 0, 100)
+    }
 
-    //lineas del piso
-    line(0, 0, 0, 0, 100, 0)
-    line(20,17, 2.5, 25, 100, 0)
-    line(40,30, 5, 50, 100, 0)
-    line(60, 45, 7.5, 75, 100, 0)
+    
+
+    
 
     if(tallBoxes){
         drawTallBoxes();
@@ -88,13 +173,37 @@ function draw() {
     else if(tallTorus){
         drawTorus();
     }
-    angle+=0.5;
+    
+  
+    if(x>55){
+      x2=-0.1;
+      y2=-0.011;
+      z2=-0.025;
+    }
+
+    if(x<3){
+      x2=0.1;
+      y2=0.011;
+      z2=0.025;
+     } 
+    x = x2+x
+    y = y2+y
+    z = z2+z
 
 
 }
 
 
 function drawTallBoxes(){  
+  
+  if(movement){
+    push();
+    translate(12+x,45+z,26+y);
+    fill(120);
+    box(10, 10, 50);
+    pop();  
+  }else{
+    
   
   if(compare){
     //figura del fondo
@@ -113,7 +222,7 @@ function drawTallBoxes(){
   }else{
    //figura del fondo
     push();
-    translate(8,18,26);
+    translate(12,45,26);
     fill(120);
     box(10, 10, 50);
     pop();
@@ -125,10 +234,21 @@ function drawTallBoxes(){
     box(10, 10, 50);
     pop(); 
   }
+  }
        
 }
 
-function drawTallCones(){  
+function drawTallCones(){ 
+  if(movement){
+    push();
+    translate(12+x,45+z,26+y);
+    rotateXaxis(-90)
+    fill(0);
+    cone(5, 50);  
+    pop();
+  }else{
+    
+  
   if(compare){
     //figura del fondo
     push();
@@ -148,7 +268,7 @@ function drawTallCones(){
   }else{
    //figura del fondo
     push();
-    translate(8,18,26);
+    translate(12,45,26);
     rotateXaxis(-90)
     fill(0);
     cone(5, 50);  
@@ -162,10 +282,20 @@ function drawTallCones(){
     cone(5, 50);
     pop();    
   }
-    
+  }
 }
 
 function drawTorus(){  
+  
+  if(movement){
+    push();
+      translate(20+(x*0.8),30+(z*2.5),15+y);
+      rotateXaxis(-90);
+      fill(0);
+      torus(10, 4); 
+      pop();
+  }else{
+    
   
     if(compare){
       //figura del fondo
@@ -186,7 +316,7 @@ function drawTorus(){
     }else{
       //figura del fondo
       push();
-      translate(15,25,15);
+      translate(20,30,15);
       rotateXaxis(-90);
       fill(0);
       torus(10, 4); 
@@ -200,7 +330,7 @@ function drawTorus(){
       torus(10, 4);
       pop();  
     }
-         
+  } 
 }
 
 
@@ -233,4 +363,23 @@ function keyPressed() {
             compare = !compare          
             break;
     }
+}
+
+function changeLeft() {
+  leftW = !leftW;
+}
+function changeRight() {
+  rightW = !rightW;
+}
+function changeCeiling() {
+  ceiling = !ceiling;
+}
+function changeFloor() {
+  floorH = !floorH;
+}
+function changeBack() {
+  back = !back;
+}
+function changeMovement() {
+  movement = !movement;
 }
