13ba404e8656b4da02933372b2f9f9a79b0a7564
ArturoQuevedo <ArturoQuevedo@users.noreply.github.com>
2022-11-19 20:00:58 +0000

    deploy: 0a52b9a187af2251a61896b002c388100f969373

--- sketches/shaders/ascii_art.js
+++ sketches/shaders/ascii_art.js
@@ -1,41 +1,93 @@
 const density='Ñ@#9876543210?!abc;:+=-,._ ';
-let cat_image;
 
-function preload(){
-  cat_image=loadImage("https://i.imgur.com/YncCi2N.jpg")
+let img;
+let img_idx=0;
+let size = 7;
+let startx = 0;
+let starty = 0;
+let ascii=false;
+
+let images=[];
+
+function preload() {
+  //img = loadImage('https://i.imgur.com/LcG0R9w.jpg');
+  let imag1 = loadImage('https://i.imgur.com/LcG0R9w.jpg');
+  let imag2 = loadImage('https://i.imgur.com/Le3VmJg.jpg');
+  let imag3 = loadImage('https://i.imgur.com/FYIILsy.jpg');
+  let imag4 = loadImage('https://i.imgur.com/klEoYK0.jpg');
+  let imag5 = loadImage('https://i.imgur.com/uQUXeDa.jpg');
+  let imag6 = loadImage('https://i.imgur.com/M0vfx1G.jpg');
+  let imag7 = loadImage('https://i.imgur.com/KRLc2lP.jpg');
+  images=[imag1,imag2,imag3,imag4,imag5,imag6,imag7]
 }
+
 function setup() {
-  createCanvas(700, 392);
+  createCanvas(400, 400);
+  slider=createSlider(3, 70, 3, 2);  
+  slider.position(2, 10);
+  
+  checkbox = createCheckbox('ascii', false);
+  checkbox.changed(myCheckedEvent);
+  checkbox.position(2, 30);
+  checkbox.style('color', 'white');
+}
+
+function myCheckedEvent() {
+  if (checkbox.checked()) {
+    ascii=true;
+  } else {
+    ascii=false;
+  }
+}
+
+function keyPressed() {
+  switch (key) {
+    case "c":
+      img_idx=(img_idx+1)%7;
+      break;
+  }
 }
 
 function draw() {
   background(0);
-  //image(cat_image, 0,0, width,height);
-  cat_image.loadPixels();
+  imageMode(CENTER);
+
+  let img=images[img_idx];
+  img.loadPixels();
+  img.resize(400, 0);
+  img.updatePixels();
   
-  
-  let w=width/cat_image.width;
-  let h=height/cat_image.height;
-  
-  for(let i=0; i<cat_image.width;i++){
-    for(let j=0; j<cat_image.height;j++){
-      const pixel_index = (i+j*cat_image.width)*4;
-      const r = cat_image.pixels[pixel_index+0];
-      const g = cat_image.pixels[pixel_index+1];
-      const b = cat_image.pixels[pixel_index+2];      
-      const luminance = r*0.3 + g*0.59 + b*0.11;
+  let w=width/img.width;
+  let h=height/img.height;
+
+  let size = slider.value();
+
+  for (var starty = 0; starty < img.height; starty++) {
+    for (var startx = 0; startx < img.width; startx++) {
+      var index = (startx + starty * img.width) * 4;
       
-      noStroke();
-      fill(255);
-      //square(i*w,j*h,w);
+      var r = img.pixels[index + 0];
+      var g = img.pixels[index + 1];
+      var b = img.pixels[index + 2];
+      var luminance = r*0.3 + g*0.59 + b*0.11;
       
-      const density_len=density.length;
-      const charIndex= floor(map(luminance,0,255,density_len,0));
+      if (ascii==false){
+        fill(r,g,b);
+        noStroke();
+        rect(startx, starty, size, size)        
+      }      
+      else{
+        const density_len=density.length;
+        const charIndex= floor(map(luminance,0,255,density_len,0));
+        fill(255);
+        textSize(size);
+        textAlign(CENTER,CENTER);
+        text(density.charAt(charIndex),startx*w+w*0.5,starty*h+h*0.5);
+      }
       
-      textSize(w);
-      textAlign(CENTER,CENTER);
-      text(density.charAt(charIndex),i*w+w*0.5,j*h+h*0.5);
+      startx = startx + size -1
     }
+    starty = starty + size -1
   }
-  
+  console.log(size);
 }
